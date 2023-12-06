807ee69ba885b740fbe43b376d0b4b8a08767535
bertieucbs <nitin.eusebius@gmail.com>
2023-12-05 19:01:27 -0800

    adding support for latest models

--- gen-ai-playgrounds/bedrock/utils/llm_type_selections/llm_configparser/amz_brck_llm_cfg.py
+++ gen-ai-playgrounds/bedrock/utils/llm_type_selections/llm_configparser/amz_brck_llm_cfg.py
@@ -26,9 +26,41 @@ import configparser
 config = configparser.ConfigParser()
 config.read('llm_config.ini')
 
-def interactWithLLM(prompt,type,bedrock_client):
+def format_text_claud(text):
+    prefix = "Human: " 
+    suffix = " Assistant: "
+    
+    formatted_text = prefix + text + suffix
+    return formatted_text
 
-    print("**THE LLM TYPE IS -->" + type)
+def format_text_llama(text):
+    prefix = "[INST] " 
+    suffix = " [/INST]"
+    
+    formatted_text = prefix + text + suffix
+    return formatted_text
+
+def interactWithLLM(prompt,type,bedrock_client):
+    
+    print("**THE LLM TYPE SELECTED IS -->" + type)
+    print("\n")
+    
+    #claude and llama needs to have a specific format for the prompt
+    if type == 'claude':
+        formatted_prompt_claud = format_text_claud(prompt)
+        print("*** Formatted text for claude ***")
+        print("\n")
+        print(formatted_prompt_claud)
+        print("\n")
+        prompt = formatted_prompt_claud
+    if type == 'llama':
+        formatted_text_llama = format_text_llama(prompt)
+        print("*** Formatted text for llama ***")
+        print("\n")
+        print(formatted_text_llama)
+        print("\n")
+        prompt = formatted_text_llama
+
 
     modelId = config[type]['modelId']
     accept = config[type]['accept']
@@ -36,7 +68,8 @@ def interactWithLLM(prompt,type,bedrock_client):
     
     body_config = config[type]['body_config']
     body_config = body_config.replace("{prompt}",prompt)
-    print("body_config---" + body_config)
+    print("body_config sending to the model API---" + body_config)
+    print("\n")
 
     body = json.dumps(json.loads(body_config,strict=False))
     
@@ -44,7 +77,8 @@ def interactWithLLM(prompt,type,bedrock_client):
         body=body, modelId=modelId, accept=accept, contentType=contentType
     )
 
-    print("response---" + str(response))
+    print("response from API ---> " + str(response))
+    print("\n")
     response_body = json.loads(response.get("body").read())
     
 
@@ -57,3 +91,10 @@ def interactWithLLM(prompt,type,bedrock_client):
     elif type == 'jurassic':
         response_text_jurassic = response_body.get('completions')[0].get("data").get("text")
         return response_text_jurassic
+    elif type == 'cohere':
+        print(response_body)
+        response_text_cohere = response_body.get('generations')[0].get("text")
+        return response_text_cohere
+    elif type == 'llama':
+        response_text_llama = response_body.get('generation')
+        return response_text_llama
